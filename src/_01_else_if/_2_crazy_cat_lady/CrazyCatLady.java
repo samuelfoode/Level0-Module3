9ec041a73fe1f62b21cf2cdff38d00d0202483e3
league <league@localhost>
2025-11-07 16:56:37 -0800

    lmjipjiop

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,12 +3,25 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String cat = JOptionPane.showInputDialog(null, "how many cats do you have");
 		// 2. Convert their answer into an int
+int catst = Integer.parseInt(cat);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if (catst>=3) {
+	JOptionPane.showMessageDialog(null,"you are a crazy cat lady");
+}
+if (catst<3&&catst>0) {
+	playVideo("https://www.youtube.com/watch?v=dd317w9MYXE");
+}
+if (catst==0) {
+	playVideo("https://www.youtube.com/watch?v=dd317w9MYXE");
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
